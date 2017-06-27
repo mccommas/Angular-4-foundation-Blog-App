81a27aaeff473d0f92440281638575d894416ac8
mccommas <jeffmccommas@gmail.com>
2017-06-27 08:53:36 -0400

    fixed error in signup method

--- src/app/admin/login/login.component.ts
+++ src/app/admin/login/login.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {UserService} from '../admin-shared/user.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
 
-  constructor() { }
+export class LoginComponent {
+    email: string;
+    password1: string;
 
-  ngOnInit() {
-  }
+    constructor(private userSVC: UserService, private router: Router){}
+
+    login(){
+        this.userSVC.login(this.email, this.password1);
+        this.userSVC.verifyUser();
+    }
+    signup(){
+        this.router.navigate(['/admin/signup']);
+    }
+    cancel(){
+        this.router.navigate(['']);
+    }
+
+
 
 }
